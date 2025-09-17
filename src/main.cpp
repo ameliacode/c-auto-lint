22f8de4de057db10c6d0243870693d7627d95b35
ameliacode <ameliacode@hanyang.ac.kr>
2025-09-17 13:06:59 +0900

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,10 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
 int main() {
-  vector<int> nums = {5, 3, 8, 1, 2};
-  sort(nums.begin(), nums.end(), [](int a, int b) { return a > b; });
-  for (auto n : nums) {
-    cout << "Number: " << n << "\n";
+  vector<int> v = {3, 1, 2};
+  for (auto x : v) {
+    cout << x << " ";
   }
-  cout << "Done!" << endl;
-  return 0;
+  cout << endl;
 }
